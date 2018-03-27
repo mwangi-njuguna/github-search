e20cee1490784c09753ee05588d21bdda4e90bc4
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-03-28 02:40:37 +0300

    created variables , arrays and objects to be used in the api service

--- src/app/apirequest.service.ts
+++ src/app/apirequest.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ApirequestService {
-
+  loading: boolean;
+  message_status: boolean;
+  item_bag: SearchItem;
+  item_store: Array<any>;
+  item_store1: Array<SearchItem>;
+  results: Object[];
+  results1: Object[];
+  results2: Object[];
+  results3: Object[];
+  message: string;
   constructor() { }
 
 }
