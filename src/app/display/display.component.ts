cfc04ad3616edcf811856777a2ab439e316fda39
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-03-28 02:31:35 +0300

    imported the apiservice service and registered it on the display component

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApirequestService } from '../apirequest.service';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.css']
+  styleUrls: ['./display.component.css'],
+  providers: [ApirequestService]
 })
 export class DisplayComponent implements OnInit {
 
